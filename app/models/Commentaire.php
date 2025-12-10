07ed65a6400725a0ee693dafaedf4e8d485daab8
Germaneau Noah <germaneaunoah@gmail.com>
2025-12-10 10:27:47 +0100

    add model commentaire

--- app/models/Commentaire.php
+++ app/models/Commentaire.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'Database.php';
 
-class Message
+class Commentaire
 {
     private $conn;
 
@@ -14,14 +14,14 @@ class Message
 
     public function findAll()
     {
-        $stmt = $this->conn->prepare("SELECT * FROM posts");
+        $stmt = $this->conn->prepare("SELECT * FROM comments");
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
     public function find($id)
     {
-        $stmt = $this->conn->prepare("SELECT * FROM posts WHERE id = :id");
+        $stmt = $this->conn->prepare("SELECT * FROM comments WHERE id = :id");
         $stmt->bindParam(':id', $id);
         $stmt->execute();
         return $stmt->fetch(PDO::FETCH_ASSOC);
@@ -29,7 +29,7 @@ class Message
 
     public function findBy(array $params)
     {
-        $query = "SELECT * FROM posts WHERE " . implode(
+        $query = "SELECT * FROM comments WHERE " . implode(
             " AND",
             array_map(
                 function ($key) {
@@ -50,28 +50,28 @@ class Message
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function add($titre, $contenu, $utilisateur_id)
+    public function add($contenu, $utilisateur_id, $post_id)
     {
-        $stmt = $this->conn->prepare("INSERT INTO posts (titre, contenu, utilisateur_id, date_publication) VALUES (:titre, :contenu, :utilisateur_id, NOW())");
-        $stmt->bindParam(':titre', $titre);
+        $stmt = $this->conn->prepare("INSERT INTO comments (contenu, utilisateur_id, post_id, date_commentaire) VALUES (:contenu, :utilisateur_id, :post_id, NOW())");
         $stmt->bindParam(':contenu', $contenu);
         $stmt->bindParam(':utilisateur_id', $utilisateur_id);
+        $stmt->bindParam(':post_id', $post_id);
         return $stmt->execute();
     }
 
-    public function update($id, $titre, $contenu, $utilisateur_id)
+    public function update($id, $contenu, $utilisateur_id, $post_id)
     {
-        $stmt = $this->conn->prepare("UPDATE posts SET titre = :titre, contenu = :contenu, utilisateur_id = :utilisateur_id WHERE id = :id");
+        $stmt = $this->conn->prepare("UPDATE comments SET contenu = :contenu, utilisateur_id = :utilisateur_id, post_id = :post_id WHERE id = :id");
         $stmt->bindParam(':id', $id);
-        $stmt->bindParam(':titre', $titre);
         $stmt->bindParam(':contenu', $contenu);
         $stmt->bindParam(':utilisateur_id', $utilisateur_id);
+        $stmt->bindParam(':post_id', $post_id);
         return $stmt->execute();
     }
 
     public function delete($id)
     {
-        $stmt = $this->conn->prepare("DELETE FROM posts WHERE id = :id");
+        $stmt = $this->conn->prepare("DELETE FROM comments WHERE id = :id");
         $stmt->bindParam(':id', $id);
         return $stmt->execute();
     }
