4bcaae2246cab2dd53f7acef18d7838cbc7a603f
Anton-Hladyshev <154468210+Anton-Hladyshev@users.noreply.github.com>
2025-12-10 10:13:48 +0100

    add header_veiw and navbar, add routes in index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once __DIR__.DIRECTORY_SEPARATOR.'app'.DIRECTORY_SEPARATOR.'views'.DIRECTORY_SEPARATOR.'header_view.php';
+require_once __DIR__.DIRECTORY_SEPARATOR.'app'.DIRECTORY_SEPARATOR.'controllers'.DIRECTORY_SEPARATOR.'UserController.php';
+
+$controller = isset($_GET['c']) ? $_GET['c'] : 'home';
+$action = isset($_GET['a']) ? $_GET['a'] : 'index';
+
+
+switch (strtolower($controller)) {
+    case 'user':
+        $userController = new UserController();
+        switch (strtolower($action)) {
+            case 'inscription':
+                $userController->getInscriptionView();
+                break;
+            case 'creer':
+                $userController->createUser();
+                break;
+            case 'connexion':
+                $userController->getLoginView();
+                break;
+            case 'login':
+                $userController->loginUser();
+                break;
+            default:
+                echo "Action non reconnue pour UserController.";
+        }
+        break;
+}
--- public/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'app'.DIRECTORY_SEPARATOR.'views'.DIRECTORY_SEPARATOR.'inscription_view.php';
-require_once __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'app'.DIRECTORY_SEPARATOR.'views'.DIRECTORY_SEPARATOR.'connexion_view.php';
-
-$controller = isset($_GET['c']) ? $_GET['c'] : 'home';
-$action = isset($_GET['a']) ? $_GET['a'] : 'index';
-
