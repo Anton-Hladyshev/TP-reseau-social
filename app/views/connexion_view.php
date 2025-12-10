b923e6208bacb25bb91172d81e7e1c20bc981974
Anton-Hladyshev <154468210+Anton-Hladyshev@users.noreply.github.com>
2025-12-10 09:32:10 +0100

    add sign up and log in pages

---
+++ app/views/connexion_view.php
@@ -0,0 +1,10 @@
+<h1>Connexion</h1>
+<form action="/connexion" method="POST">
+    <label for="email">Adresse e-mail:</label>
+    <input type="email" id="email" name="email" required>
+
+    <label for="password">Mot de passe:</label>
+    <input type="password" id="password" name="password" required>
+
+    <button type="submit">Se connecter</button>
+</form>
