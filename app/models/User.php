8a0746de2cf0847c606de8db8b2e0602f07c5dc6
Anton-Hladyshev <154468210+Anton-Hladyshev@users.noreply.github.com>
2025-12-10 09:42:14 +0100

    add User and Database connection modes

---
+++ app/models/User.php
@@ -0,0 +1,69 @@
+<?php
+
+require_once __DIR__.DIRECTORY_SEPARATOR.'Database.php';
+
+class User {
+    private $conn;
+
+    public function __construct($id, $username, $email, $password) {
+        $database = new Database();
+        $this->conn = $database->getConnection();
+    }
+
+    public function findAll() {
+        $stmt = $this->conn->prepare("SELECT * FROM users");
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function find($id) {
+        $stmt = $this->conn->prepare("SELECT * FROM users WHERE id = :id");
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function findBy(array $params) {
+        $query = "SELECT * FROM users WHERE " . implode(
+            " AND" , 
+            array_map(
+                function($key) {
+                    return "$key = :$key";
+                },
+                array_keys($params)
+            )
+        );
+
+        $stmt = $this->conn->prepare($query);
+        foreach ($params as $key => $value) {
+            $stmt->bindValue(
+                ":$key", $value
+            );
+        }
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function add($username, $email, $password) {
+        $stmt = $this->conn->prepare("INSERT INTO users (username, email, password) VALUES (:username, :email, :password)");
+        $stmt->bindParam(':username', $username);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':password', password_hash($password, PASSWORD_BCRYPT));
+        return $stmt->execute();
+    }
+
+    public function update($id, $username, $email, $password) {
+        $stmt = $this->conn->prepare("UPDATE users SET username = :username, email = :email, password = :password WHERE id = :id");
+        $stmt->bindParam(':id', $id);
+        $stmt->bindParam(':username', $username);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':password', password_hash($password, PASSWORD_BCRYPT));
+        return $stmt->execute();
+    }
+
+    public function delete($id) {
+        $stmt = $this->conn->prepare("DELETE FROM users WHERE id = :id");
+        $stmt->bindParam(':id', $id);
+        return $stmt->execute();
+    }   
+}
